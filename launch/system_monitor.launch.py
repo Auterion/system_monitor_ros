a5fe81c3eb50e2048ea971a7ab398741c67ac9fd
TSC21 <n.marques21@hotmail.com>
2020-01-17 16:55:05 +0000

    pass parameters file using the path of the script itself

--- launch/system_monitor.launch.py
+++ launch/system_monitor.launch.py
@@ -3,13 +3,16 @@
 Launch system_monitor_node
 """
 
+import os
 from launch import LaunchDescription
 import launch_ros.actions
 
 
 def generate_launch_description():
+    parameters_file_path = os.path.dirname(os.path.realpath(__file__))
+
     return LaunchDescription([
         launch_ros.actions.Node(
             package='system_monitor_ros', node_executable='system_monitor_node', output='screen',
-            parameters=["install/system_monitor_ros/share/system_monitor_ros/launch/monitor_parameters.yaml"]),
+            parameters=[parameters_file_path + "/monitor_parameters.yaml"]),
     ])
